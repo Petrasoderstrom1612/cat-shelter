b55c63ff9e892464e9e543b1d168ddb698c3690a
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-04-03 15:45:27 +0200

    finished static site

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Entry from "./Entry"
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1 style={{textAlign: "center"}}>Söderströms' cat shelter</h1>
+      <div className="contacts">
+        <Entry img="./src/images/mr-whiskerson.png" name="Mr. Whiskerson" phone="(212) 555-1234" email="mr.whiskaz@catnap.meow"/>
+        <Entry img="./src/images/fluffykins.png" name="Fluffykins" phone="(212) 555-2345" email="fluff@me.com"/>
+        <Entry img="./src/images/felix.png" name="Felix" phone="(212) 555-4567" email="thecat@hotmail.com"/>
+        <Entry img="./src/images/pumpkin.png" name="Pumpkin" phone="(0800) CAT KING" email="pumpkin@scrimba.com"/>
+        </div>
     </>
   )
 }
