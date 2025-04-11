553129d944ea7da5ea213b75aa0445fd8a166e31
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-04-11 09:05:15 +0200

    cats

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ function App() {
     <>
     <h1 style={{textAlign: "center"}}>Söderströms' cat shelter</h1>
       <div className="contacts">
-        <Entry img="./src/images/mr-whiskerson.png" name="Mr. Whiskerson" phone="(212) 555-1234" email="mr.whiskaz@catnap.meow"/>
-        <Entry img="./src/images/fluffykins.png" name="Fluffykins" phone="(212) 555-2345" email="fluff@me.com"/>
-        <Entry img="./src/images/felix.png" name="Felix" phone="(212) 555-4567" email="thecat@hotmail.com"/>
-        <Entry img="./src/images/pumpkin.png" name="Pumpkin" phone="(0800) CAT KING" email="pumpkin@scrimba.com"/>
+        <Entry img="/src/images/mr-whiskerson.png" name="Mr. Whiskerson" phone="(212) 555-1234" email="mr.whiskaz@catnap.meow"/>
+        <Entry img="/src/images/fluffykins.png" name="Fluffykins" phone="(212) 555-2345" email="fluff@me.com"/>
+        <Entry img="/src/images/felix.png" name="Felix" phone="(212) 555-4567" email="thecat@hotmail.com"/>
+        <Entry img="/src/images/pumpkin.png" name="Pumpkin" phone="(0800) CAT KING" email="pumpkin@scrimba.com"/>
         </div>
     </>
   )
 }
 
-export default App
+export default App 
