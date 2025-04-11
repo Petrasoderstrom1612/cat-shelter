861df564307bcda05f06bec3abd5e2e02faf69d2
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-04-11 09:08:31 +0200

    imported images of cats

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import Entry from "./Entry"
+import Whiskerson from "/src/images/mr-whiskerson.png"
+import Fluffykins from "/src/images/fluffykins.png"
+import Felix from "/src/images/felix.png"
+import Pumpkin from "/src/images/pumpkin.png"
 
 function App() {
 
@@ -8,10 +12,10 @@ function App() {
     <>
     <h1 style={{textAlign: "center"}}>Söderströms' cat shelter</h1>
       <div className="contacts">
-        <Entry img="/src/images/mr-whiskerson.png" name="Mr. Whiskerson" phone="(212) 555-1234" email="mr.whiskaz@catnap.meow"/>
-        <Entry img="/src/images/fluffykins.png" name="Fluffykins" phone="(212) 555-2345" email="fluff@me.com"/>
-        <Entry img="/src/images/felix.png" name="Felix" phone="(212) 555-4567" email="thecat@hotmail.com"/>
-        <Entry img="/src/images/pumpkin.png" name="Pumpkin" phone="(0800) CAT KING" email="pumpkin@scrimba.com"/>
+        <Entry img={Whiskerson} name="Mr. Whiskerson" phone="(212) 555-1234" email="mr.whiskaz@catnap.meow"/>
+        <Entry img={Fluffykins} name="Fluffykins" phone="(212) 555-2345" email="fluff@me.com"/>
+        <Entry img={Felix} name="Felix" phone="(212) 555-4567" email="thecat@hotmail.com"/>
+        <Entry img={Pumpkin} name="Pumpkin" phone="(0800) CAT KING" email="pumpkin@scrimba.com"/>
         </div>
     </>
   )
